d4662a6ccf1f430707851660db396650414611ec
Rory Standley <rorystandley@gmail.com>
2023-02-03 09:39:53 +0000

    Refactor script

--- app.js
+++ app.js
@@ -14,70 +14,170 @@ let msg = {
   subject: 'War Day Results',
 }
 
-function getRequestStuff(url) {
-  return options = {
-    'method': 'GET',
-    'url': `https://proxy.royaleapi.dev/v1/${url}`,
-    'headers': {
-      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjllNDRjZDJjLWMzMzEtNDk4ZC1hZWVjLTNiODhmODllZmEyOCIsImlhdCI6MTY1OTk2ODEwMCwic3ViIjoiZGV2ZWxvcGVyLzFlMTExZWJlLWRmZjYtMjEwYy0zYWI0LTBhMjNiY2U4NTQyZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS43OS4yMTguNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.LsVuAZNI0JHfOVbBldPw7cMTcMne0Ggu06-RRuwGCovCuyg8xN7naQVkqH4h_aDFnBQM2vgc1vMHv-R1HybgWg'
-    }
-  };
-}
-
-app.get('/:id?', (req, res) => {
-  if (req.params.id && req.params.id === 'rory') {
-    request(getRequestStuff('clans/%232L8CYUP/currentriverrace'), function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-
-        let data = JSON.parse((body)).clan.participants;
-        let html = "<table border='1' style='width:100%; border-collapse: collapse;'>";
-        html += "<tr><th>Position</th><th>Name</th><th>Fame</th><th>Repair Points</th><th>Boat Attacks</th><th>Decks Used</th><th>Decks Used Today</th></tr>"
-        let counter = 0;
-        data.forEach((element) => {
-          console.log(`players/${element.tag}`)
-          //check to see if they're current members
-          // request(getRequestStuff(`players/${encodeURIComponent(element.tag)}`), function (errorTwo, responseTwo, bodyTwo) {
-          // let check = JSON.parse((bodyTwo)).clan;
-          // console.log(check,'#2L8CYUP' )
-          // if (check && check.tag == '#2L8CYUP') {
-          html += "<tr>";
-          html += "<td style='text-align:center'>";
-          html += parseInt(data.length) - parseInt(counter);
-          html += "</td>";
-          html += "<td style='padding-left: 10px;'>";
-          html += element.name;
-          html += "</td>";
-          html += "<td style='text-align:center'>";
-          html += element.fame;
-          html += "</td>";
-          html += "<td style='text-align:center'>";
-          html += element.repairPoints;
-          html += "</td>";
-          html += "<td style='text-align:center'>";
-          html += element.boatAttacks;
-          html += "</td>";
-          html += "<td style='text-align:center'>";
-          html += element.decksUsed;
-          html += "</td>";
-          html += "<td style='text-align:center'>";
-          html += element.decksUsedToday;
-          html += "</td>";
-          html += "</tr>";
-          counter++;
-        });
-        html += "</table>"
-        msg.html = html;
-        sendgrid.send(msg).then((resp) => {
-          console.log('Email sent\n', resp)
-        }).catch((error) => {
-          console.error(JSON.stringify(error))
-        })
-        res.json("This has run fine")
-      }
-    })
-  } else {
-    res.json("This has run fine")
+const getRequestOptions = (url) => ({
+  'method': 'GET',
+  'url': `https://proxy.royaleapi.dev/v1/${url}`,
+  'headers': {
+    'Authorization': `Bearer ${process.env.CLASH_ROYALE_BEARER_TOKEN}`
   }
 });
+
+async function getRiverRaceData() {
+  try {
+    const clanData = await request(getRequestOptions('clans/%232L8CYUP/currentriverrace'));
+    const clanDataString = await new Promise((resolve, reject) => {
+      let data = '';
+      clanData.on('data', chunk => {
+        data += chunk;
+      });
+      clanData.on('end', () => {
+        resolve(data);
+      });
+      clanData.on('error', error => {
+        reject(error);
+      });
+    });
+    const clanDataJSON = JSON.parse(clanDataString);
+    const currentClanMembers = [];
+    const requests = clanDataJSON.clan.participants.map((participant, index) => {
+      return new Promise(async (resolve, reject) => {
+        await setTimeout(async () => {
+          request(getRequestOptions(`players/${encodeURIComponent(participant.tag)}`))
+            .on('response', (response) => {
+              let data = '';
+              response.on('data', dataChunk => {
+                data += dataChunk;
+              });
+              response.on('end', () => {
+                resolve(data);
+              });
+            })
+            .on('error', error => {
+              reject(error);
+            });
+        }, 500 * index);
+      });
+    });
+    const additionalData = await Promise.all(requests);
+    additionalData.forEach((data, i) => {
+      const participantDataJSON = JSON.parse(data);
+      const participantExists = participantDataJSON.clan && participantDataJSON.clan.tag === '#2L8CYUP';
+      // console.log(participantDataJSON.name, participantExists);
+      if (participantExists) {
+        currentClanMembers.push(clanDataJSON.clan.participants[i]);
+      }
+    });
+    return currentClanMembers;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+app.get('/:id?', async (req, res) => {
+  if (!req.params.id || req.params.id !== "rory") {
+    return res.status(400).send("Invalid request.");
+  }
+
+  try {
+    const data = await getRiverRaceData();
+    const html = generateHtmlTable(data);
+    msg.html = html;
+    sendgrid.send(msg);
+    res.status(200).send("Email sent.");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occured.");
+  }
+
+});
+
+function generateHtmlTable(data) {
+  let html = "<table border='1' style='width:100%; border-collapse: collapse;'>";
+  html += "<tr><th>Position</th><th>Name</th><th>Fame</th><th>Repair Points</th><th>Boat Attacks</th><th>Decks Used</th><th>Decks Used Today</th></tr>"
+  let counter = 0;
+  data.forEach((element) => {
+    html += "<tr>";
+    html += "<td style='text-align:center'>";
+    html += parseInt(data.length) - parseInt(counter);
+    html += "</td>";
+    html += "<td style='padding-left: 10px;'>";
+    html += element.name;
+    html += "</td>";
+    html += "<td style='text-align:center'>";
+    html += element.fame;
+    html += "</td>";
+    html += "<td style='text-align:center'>";
+    html += element.repairPoints;
+    html += "</td>";
+    html += "<td style='text-align:center'>";
+    html += element.boatAttacks;
+    html += "</td>";
+    html += "<td style='text-align:center'>";
+    html += element.decksUsed;
+    html += "</td>";
+    html += "<td style='text-align:center'>";
+    html += element.decksUsedToday;
+    html += "</td>";
+    html += "</tr>";
+    counter++;
+  });
+  html += "</table>"
+  return html;
+}
+//   if (req.params.id && req.params.id === 'rory') {
+//     request(getRequestStuff('clans/%232L8CYUP/currentriverrace'), function (error, response, body) {
+//       if (!error && response.statusCode == 200) {
+
+//         let data = JSON.parse((body)).clan.participants;
+//         let html = "<table border='1' style='width:100%; border-collapse: collapse;'>";
+//         html += "<tr><th>Position</th><th>Name</th><th>Fame</th><th>Repair Points</th><th>Boat Attacks</th><th>Decks Used</th><th>Decks Used Today</th></tr>"
+//         let counter = 0;
+//         data.forEach((element) => {
+//           console.log(`players/${element.tag}`)
+//           //check to see if they're current members
+//           // request(getRequestStuff(`players/${encodeURIComponent(element.tag)}`), function (errorTwo, responseTwo, bodyTwo) {
+//           // let check = JSON.parse((bodyTwo)).clan;
+//           // console.log(check,'#2L8CYUP' )
+//           // if (check && check.tag == '#2L8CYUP') {
+//           html += "<tr>";
+//           html += "<td style='text-align:center'>";
+//           html += parseInt(data.length) - parseInt(counter);
+//           html += "</td>";
+//           html += "<td style='padding-left: 10px;'>";
+//           html += element.name;
+//           html += "</td>";
+//           html += "<td style='text-align:center'>";
+//           html += element.fame;
+//           html += "</td>";
+//           html += "<td style='text-align:center'>";
+//           html += element.repairPoints;
+//           html += "</td>";
+//           html += "<td style='text-align:center'>";
+//           html += element.boatAttacks;
+//           html += "</td>";
+//           html += "<td style='text-align:center'>";
+//           html += element.decksUsed;
+//           html += "</td>";
+//           html += "<td style='text-align:center'>";
+//           html += element.decksUsedToday;
+//           html += "</td>";
+//           html += "</tr>";
+//           counter++;
+//         });
+//         html += "</table>"
+//         msg.html = html;
+//         sendgrid.send(msg).then((resp) => {
+//           console.log('Email sent\n', resp)
+//         }).catch((error) => {
+//           console.error(JSON.stringify(error))
+//         })
+//         res.json("This has run fine")
+//       }
+//     })
+//   } else {
+//     res.json("This has run fine")
+//   }
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
