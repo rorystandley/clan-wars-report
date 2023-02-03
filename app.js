bd31ae6739cb93116d88542fc79dec63d2b3c90c
Rory Standley <rorystandley@gmail.com>
2023-02-03 10:15:07 +0000

    Cleanup

--- app.js
+++ app.js
@@ -124,59 +124,5 @@ function generateHtmlTable(data) {
   html += "</table>"
   return html;
 }
-//   if (req.params.id && req.params.id === 'rory') {
-//     request(getRequestStuff('clans/%232L8CYUP/currentriverrace'), function (error, response, body) {
-//       if (!error && response.statusCode == 200) {
-
-//         let data = JSON.parse((body)).clan.participants;
-//         let html = "<table border='1' style='width:100%; border-collapse: collapse;'>";
-//         html += "<tr><th>Position</th><th>Name</th><th>Fame</th><th>Repair Points</th><th>Boat Attacks</th><th>Decks Used</th><th>Decks Used Today</th></tr>"
-//         let counter = 0;
-//         data.forEach((element) => {
-//           console.log(`players/${element.tag}`)
-//           //check to see if they're current members
-//           // request(getRequestStuff(`players/${encodeURIComponent(element.tag)}`), function (errorTwo, responseTwo, bodyTwo) {
-//           // let check = JSON.parse((bodyTwo)).clan;
-//           // console.log(check,'#2L8CYUP' )
-//           // if (check && check.tag == '#2L8CYUP') {
-//           html += "<tr>";
-//           html += "<td style='text-align:center'>";
-//           html += parseInt(data.length) - parseInt(counter);
-//           html += "</td>";
-//           html += "<td style='padding-left: 10px;'>";
-//           html += element.name;
-//           html += "</td>";
-//           html += "<td style='text-align:center'>";
-//           html += element.fame;
-//           html += "</td>";
-//           html += "<td style='text-align:center'>";
-//           html += element.repairPoints;
-//           html += "</td>";
-//           html += "<td style='text-align:center'>";
-//           html += element.boatAttacks;
-//           html += "</td>";
-//           html += "<td style='text-align:center'>";
-//           html += element.decksUsed;
-//           html += "</td>";
-//           html += "<td style='text-align:center'>";
-//           html += element.decksUsedToday;
-//           html += "</td>";
-//           html += "</tr>";
-//           counter++;
-//         });
-//         html += "</table>"
-//         msg.html = html;
-//         sendgrid.send(msg).then((resp) => {
-//           console.log('Email sent\n', resp)
-//         }).catch((error) => {
-//           console.error(JSON.stringify(error))
-//         })
-//         res.json("This has run fine")
-//       }
-//     })
-//   } else {
-//     res.json("This has run fine")
-//   }
-// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
